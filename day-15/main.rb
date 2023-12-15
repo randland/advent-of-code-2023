5781e2c7a357cad5a4cad1ca41f5bb869d983632
Nick Karpenske <randland@gmail.com>
2023-12-14 22:48:46 -0700

    :recycle: Cleaner code

--- day-15/main.rb
+++ day-15/main.rb
@@ -5,32 +5,36 @@ def file(path) = File.read(File.join(__dir__, path))
 
 def parse(data) = data.split("\n").join.split(",")
 
-def hash(str) = str.chars.reduce(0) { |score, char| (((score + char.ord) * 17) % 256) }
+def hash(str) = str.chars.reduce(0) { |val, char| (((val + char.ord) * 17) % 256) }
 
 def part1(data) = data.map(&method(:hash)).sum
 
-def part2(data)
+def populate(data)
   boxes = {}
 
   data.each do |map|
-    str, op, val = map.match(/([a-z]*)([-=])(\d*)$/).captures
+    label, op, val = map.match(/([a-z]*)([-=])(\d*)$/).captures
     val = val == "" ? nil : val.to_i
-    loc = hash(str)
+    loc = hash(label)
 
     boxes[loc] ||= {}
 
-    if op == "-"
-      boxes[loc].delete(str)
-    else
-      boxes[loc][str] = val
-    end
+    op == "-" ? boxes[loc].delete(label) : boxes[loc][label] = val
   end
 
+  boxes
+end
+
+def score(boxes)
   boxes.to_a.flat_map do |key, vals|
-    vals.flat_map.with_index do |val, idx|
+    vals.map.with_index do |val, idx|
       (key + 1) * (idx + 1) * val.last
     end
   end.sum
+end
+
+def part2(data)
+  score(populate(data))
 end
 
 EXAMPLE = parse file "example"
