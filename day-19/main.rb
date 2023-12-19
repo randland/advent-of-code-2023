ac497369ed2d9853bdbecd8410ed3d3daddcb2fb
Nick Karpenske <randland@gmail.com>
2023-12-18 23:52:47 -0700

    :recycle: Cleaner

--- day-19/main.rb
+++ day-19/main.rb
@@ -4,39 +4,8 @@ require "set"
 
 def file(path) = File.read(File.join(__dir__, path))
 
-# class Range
-#   def remove(other)
-#     # Process every value in the array
-#     if other.kind_of?(Array) then
-#       return [self] if other.empty?
-
-#       # Remove each chunk individually
-#       return other.map(&method(:remove)).inject do |a, b|
-#         # Join the intersections of all removed bits
-#         a.product(b).filter_map { |x, y| x.intersect(y) }
-#       end
-#     end
-
-#     # Other covers nothing
-#     return [self] if other.nil? || other.max < min || other.min > max
-
-#     # Other covers everything
-#     return [] if other.min <= min && other.max >= max
-
-#     # Other covers start of range
-#     return [other.max+1..max] if other.min <= min && other.max < max
-
-#     # Other covers end of range
-#     return [min..other.min-1] if other.min > min && other.max >= max
-
-#     [min..other.min-1, other.max+1..max]
-#   end
-# end
-
 class Hash
-  def deep_dup
-    transform_keys(&:dup)
-  end
+  def deep_dup = transform_keys(&:dup)
 end
 
 class WorkflowData
@@ -51,37 +20,22 @@ class WorkflowData
     return 0 if loc == "R"
     return ranges.values.map { |a, b| b - a + 1 }.inject(:*) if loc == "A"
 
-    combs = 0
+    count = 0
     workflows[loc].flow.each do |step|
       old_min, old_max = ranges[step.var]
       case step.op
       when "<"
-        if old_min < step.val
-          new_ranges = ranges.dup
-          new_ranges[step.var] = [old_min, [old_max, step.val - 1].min]
-          combs += find_comb(step.dest, new_ranges)
-        end
-
-        if old_max >= step.val
-          ranges[step.var] = [[old_min, step.val].max, old_max]
-        else
-          break
-        end
+        sub_ranges = ranges.merge(step.var => [old_min, step.val - 1])
+        count += find_comb(step.dest, sub_ranges) if old_min < step.val
+        ranges[step.var] = [step.val, old_max] if old_max >= step.val
       when ">"
-        if old_max > step.val
-          new_ranges = ranges.merge(step.var => [[old_min, step.val + 1].max, old_max])
-          combs += find_comb(step.dest, new_ranges)
-        end
-
-        if old_min <= step.val
-          ranges[step.var] = [old_min, [old_max, step.val].min]
-        else
-          break
-        end
+        sub_ranges = ranges.merge(step.var => [step.val + 1, old_max])
+        count += find_comb(step.dest, sub_ranges) if old_max > step.val
+        ranges[step.var] = [old_min, step.val] if old_min <= step.val
       end
     end
 
-    combs += find_comb(workflows[loc].default, ranges.deep_dup)
+    count + find_comb(workflows[loc].default, ranges.deep_dup)
   end
 end
 
@@ -130,41 +84,17 @@ def parse(data)
   end
 end
 
-class OptionSet
-  attr_reader :ranges
-
-  def initialize
-    @ranges = {
-      "x" => [1, 4000],
-      "m" => [1, 4000],
-      "a" => [1, 4000],
-      "s" => [1, 4000]
-    }
-  end
-
-  def restrict(var, op, val)
-    case op
-    when ">"
-      ranges[var] = [[ranges[var].first, val].max, ranges[var].last]
-    when "<"
-      ranges[var] = [ranges[var].first, [ranges[var].last, val].min]
-    end
-  end
-
-  def combinations
-    ranges.values.map(&:count).inject(:*)
-  end
-end
-
 def part1(data)
   accepted = Set.new
   rejected = Set.new
 
   data.parts.each do |part|
     loc = "in"
+
     until %w[A R].include?(loc)
       loc = data.workflows[loc].process(part)
     end
+
     loc == "A" ? accepted << part : rejected << part
   end
 
@@ -172,7 +102,15 @@ def part1(data)
 end
 
 def part2(data)
-  data.find_comb("in", { "x" => [1, 4000], "m" => [1, 4000], "a" => [1, 4000], "s" => [1, 4000] })
+  data.find_comb(
+    "in",
+    {
+      "x" => [1, 4000],
+      "m" => [1, 4000],
+      "a" => [1, 4000],
+      "s" => [1, 4000]
+    }
+  )
 end
 
 EXAMPLE = parse file "example"
